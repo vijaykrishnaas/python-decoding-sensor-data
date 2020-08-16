eafea5ce05c6fbb3ae3e2f3d66b5fb3236ced7a1
vijaykrishnaa <vk596466@gmail.com>
2020-08-16 23:37:34 +0530

    Completed module

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -18,6 +18,6 @@ class HouseInfo:
     def get_data_by_date(self, field, rec_date=date.today()):
         field_data = []
         for record in self.data:
-            if record['date'] == date.strftime(rec_date):
+            if record['date'] == date.strptime(rec_date):
                 field_data.append(record[field])
         return field_data
--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -20,7 +20,7 @@ test_area = 1
 recs = house_info.get_data_by_area("id", rec_area=test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
-test_date = datetime.strftime("5/9/20", "%m/%d/%y")
+test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
